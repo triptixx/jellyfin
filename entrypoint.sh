305e062e986ba6774c9e215cd928ec549d1985f7
Triptixx <54292597+triptixx@users.noreply.github.com>
2024-01-16 17:59:49 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,20 +7,16 @@ RESET='\033[0m'
 
 CONFIG_DIR='/config'
 
-for DIR in `echo $CONFIG_DIR | tr ';' '\n'`; do
-    mkdir -p "$DIR"
+if gosu $SUID:$SGID [ ! -w "$CONFIG_DIR" ]; then
+    2>&1 echo -e "${RED}####################### WARNING #######################${RESET}"
+    2>&1 echo
+    2>&1 echo -e "${RED}     No permission to write in '$CONFIG_DIR' directory.${RESET}"
+    2>&1 echo -e "${RED}       Correcting permissions to prevent a crash.${RESET}"
+    2>&1 echo
+    2>&1 echo -e "${RED}#######################################################${RESET}"
+    2>&1 echo
 
-    if gosu $SUID:$SGID [ ! -w "$DIR" ]; then
-        2>&1 echo -e "${RED}####################### WARNING #######################${RESET}"
-        2>&1 echo
-        2>&1 echo -e "${RED}     No permission to write in '$DIR' directory.${RESET}"
-        2>&1 echo -e "${RED}       Correcting permissions to prevent a crash.${RESET}"
-        2>&1 echo
-        2>&1 echo -e "${RED}#######################################################${RESET}"
-        2>&1 echo
-
-        chown $SUID:$SGID "$DIR"
-    fi
-done
+    chown $SUID:$SGID "$CONFIG_DIR"
+fi
 
 exec gosu $SUID:$SGID "$@"
